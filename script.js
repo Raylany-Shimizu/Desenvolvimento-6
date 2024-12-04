4a6c8ed0511261000bb39f88b4d94b39198be63b
Raylany <materialdeestudo39@gmail.com>
2024-12-04 14:35:45 -0300

    Desenvolvimento 6

---
+++ script.js
@@ -0,0 +1,13 @@
+document.body.innerHTML += '<h1 id="titulo">Seja bem vindo à loja XYZ!</h1>';
+
+document.body.innerHTML += `
+  <div id="produto">
+    <h2>TV 32'</h2>
+    <p>Uma televisão Smart, com ótima resolução e com garantia absoluta!</p>
+    <p>Preço: R$ 3.500,00</p>
+  </div>
+`;
+document.body.innerHTML += `
+  <img src="tv.webp" alt="Imagem de TV">
+`;
+
